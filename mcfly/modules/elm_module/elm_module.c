16eee793329e3c47e64d26f2b91ff781f469c89f
mdavis <mdavis@dragonrun.(none)>
2009-12-10 22:57:12 -0500

    Initalize (or try to) the elm module

--- mcfly/modules/elm_module/elm_module.c
+++ mcfly/modules/elm_module/elm_module.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include <mcfly/error.h>
+#include <mcfly/config.h>
+#include <mcfly/mcfly.h>
 #include <mcfly/type.h>
 #include "elm327.h"
 
@@ -13,8 +15,38 @@
  */
 
 
+/* Device descriptor */
+static int elm_mod_fd = -1;
+
+
+/* Default device path */
+#define ELM_MOD_DEFAULT_DEV "/dev/ttyUSB0"
+
+
+/* Configurations */
+#define ELM_MOD_CFG_DEV_PATH 1
+static const mcfly_cfg_def_t elm_mod_configs[] = 
+{
+    {"elm_mod_dev_path", "Path to ELM device",
+     ELM_MOD_CFG_DEV_PATH, MCFLY_CFG_VALUE_STRING},
+};
+
+
 static mcfly_err_t elm_init(const mcfly_t mcfly, mcfly_mod_t *me)
 {
+    const char *dev;
+
+    /* Load the configs */
+    me->configs = mcfly_cfg_load(
+        mcfly, elm_mod_configs, MCFLY_ARRAY_SIZE(elm_mod_configs), NULL, NULL);
+
+    /* Look for the device path from the configs */
+    if (!(dev = mcfly_cfg_get_from_name(me->configs, "elm_mod_dev_path")))
+      dev = ELM_MOD_DEFAULT_DEV;
+
+    if ((elm_mod_fd = elm327_init(dev)) < 0)
+      return MCFLY_ERR_MODINIT;
+
     return MCFLY_SUCCESS;
 }
 
