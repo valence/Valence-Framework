27e66d729f1c7df4eb3983a1a304ddd50828da74
mdavis <mdavis@dragonrun.(none)>
2009-12-22 19:05:46 -0500

    Add cpp binding tests

---
+++ mcfly/bindings/cpp/tests/main.cc
@@ -0,0 +1,22 @@
+#include <mcfly_cpp.hh>
+using namespace mcfly;
+
+
+int main(void)
+{
+    Mcfly *marty = new Mcfly("test_config.cfg");
+
+    // Test: Initialization
+    if (marty->getError() != MCFLY_SUCCESS)
+      return marty->getErrorValue();
+
+    // Test: Command
+    mcfly_mod_data_t data;
+    if (marty->command(MCFLY_MOD_TYPE_OBD, 
+            MCFLY_MOD_CMD_OBD_SPEED, &data) != MCFLY_SUCCESS)
+    {
+        return marty->getErrorValue();
+    }
+
+    return 0;
+}
